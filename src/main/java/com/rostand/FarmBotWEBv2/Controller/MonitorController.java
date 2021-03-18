6841edc6a390be2de2c83b979848a333f5cb3bcc
Nina Alin <alin.nina28@gmail.com>
2021-03-18 20:48:14 +0100

    Version 18/03/21

--- src/main/java/com/rostand/FarmBotWEBv2/Controller/MonitorController.java
+++ src/main/java/com/rostand/FarmBotWEBv2/Controller/MonitorController.java
@@ -1,13 +1,22 @@
 package com.rostand.FarmBotWEBv2.Controller;
 
+import com.rostand.FarmBotWEBv2.Repository.MonitorRepository;
 import com.rostand.FarmBotWEBv2.SerialCommunication.SerialFarmBot;
 import jssc.SerialPortException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
+@RestController
 public class MonitorController {
 
-        // -------------------------------- PARTIE AXE X ---------------------------------
+    @Autowired
+    MonitorRepository monitorRepository;
+
+    // -------------------------------- PARTIE AXE X ---------------------------------
 
         @GetMapping(value = "/monitor/axeX/getPositionAxeX")
         public int getPositionAxeX () {
@@ -87,6 +96,22 @@ public class MonitorController {
 
             farmbot.envoyerOrdre("F41 P7 V1 M0 Q0");
         }
+
+    @PostMapping(value = "/monitor/actionneurs/lumiere/off")
+    public void lumiereOff () throws SerialPortException {
+
+        SerialFarmBot farmbot = null;
+        int posX = 0, posY = 0, posZ = 0;
+        String shell = System.getenv("SHELL");
+
+        try {
+            farmbot = SerialFarmBot.getInstance("/dev/ttyACM0");
+        } catch (Exception er) {
+            er.printStackTrace();
+        }
+
+        farmbot.envoyerOrdre("F41 P7 V0 M0 Q0");
+    }
 
         // --------------------------------------- CAMERA ---------------------------------------------
 
--- src/main/java/com/rostand/FarmBotWEBv2/Entity/Monitor.java
+++ src/main/java/com/rostand/FarmBotWEBv2/Entity/Monitor.java
@@ -4,9 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @Setter
 @Entity
 public class Monitor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long Id;
 }
